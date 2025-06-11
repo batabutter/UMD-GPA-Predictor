94c14151f8c834b9fc1c35c4be2b610641430a97
bata_butter <113527212+batabutter@users.noreply.github.com>
2025-06-11 03:45:52 -0400

    Moved some parts of the data intialization

--- GPATrend/src/App.jsx
+++ GPATrend/src/App.jsx
@@ -3,94 +3,118 @@ import { Chart as ChartJS } from "chart.js/auto";
 import { Bar } from "react-chartjs-2"
 import './styles.css'
 import './App.css'
-import {SectionDistribution} from "./SectionDist"
+import { SectionDistribution } from "./SectionDist"
 import { TotalDistribution } from './TotalDistribution';
 import { GPATrend } from './GPATrend';
 
 function App() {
   const [data, setData] = useState([{}])
-  const [gradeData, setGradeData] = useState([{}])
+  const [sectionData, setSectionData] = useState([{}])
   const [totalGradeData, setTotalGradeData] = useState([{}])
 
   // Start by fetching basic user data information
   useEffect(() => {
-  fetch("/course_info")
-    .then(res => res.json())
-    .then(data => {
-      setData(data)
-    })
+    fetch("/course_info")
+      .then(res => res.json())
+      .then(data => {
+        setData(data)
+      })
 
-}, []);
-
-  useEffect(() => {
-  fetch("/course_grade_dis")
-    .then(res => res.json())
-    .then(data => {
-      const raw = data[0];
-      const gradeKeys = Object.keys(raw).filter(
-        key => !["course", "professor", "section", "semester"].includes(key)
-      );
-
-      const gradeArray = gradeKeys.map(grade => ({
-        grade,
-        count: raw[grade],
-      }));
-
-      setGradeData(gradeArray);
-      console.log(gradeArray);
-    });
   }, []);
 
   useEffect(() => {
-  fetch("/course_grade_dis")
-    .then(res => res.json())
-    .then(data => {
-      const cumulativeGrades = {};
+    fetch("/course_grade_dis")
+      .then(res => res.json())
+      .then(data => {
 
-      data.forEach(item => {
-        Object.keys(item).forEach(key => {
-        if (!["Other", "course", "professor", "section", "semester"].includes(key)) {
-          if (!cumulativeGrades[key]) 
-            cumulativeGrades[key] = 0;
-          cumulativeGrades[key] += item[key]
-        }
+        let formatted_year = ""
+        let formatted_info = ""
+
+        const grade_dis = data.map(item => {
+
+          const info = {
+            "A": 0, "A+": 0, "A-": 0,
+            "B": 0, "B+": 0, "B-": 0,
+            "C": 0, "C+": 0, "C-": 0,
+            "D": 0, "D+": 0, "D-": 0,
+            "F": 0, "Other": 0, "W": 0,
+            "course": "", "professor": "",
+            "section": "", "semester": "",
+            "formatted_name": ""
+          };
+
+          formatted_year = item.semester.slice(0, 4)
+          if (item.semester[item.semester.length - 1] == "1")
+            formatted_year = "Fall " + formatted_year
+          else
+            formatted_year = "Spring " + formatted_year
+
+          formatted_info = formatted_year + ", " + item.section
+            + " - " + item.professor
+
+          Object.keys(item).forEach(key => {
+            info[key] = item[key]
+            info["formatted_name"] = formatted_info
+          })
+
+          return info
         })
-      });
-    
-      const gradeArr = Object.entries(cumulativeGrades).map(([grade, count]) => ({
-        grade,
-        count
-      }));
 
-      setTotalGradeData(gradeArr);
-    });
-}, []);
+        setSectionData(grade_dis)
+        console.log(grade_dis)
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch("/course_grade_dis")
+      .then(res => res.json())
+      .then(data => {
+        const cumulativeGrades = {};
+
+        data.forEach(item => {
+          Object.keys(item).forEach(key => {
+            if (!["Other", "course", "professor", "section", "semester"].includes(key)) {
+              if (!cumulativeGrades[key])
+                cumulativeGrades[key] = 0;
+              cumulativeGrades[key] += item[key]
+            }
+          })
+        });
+
+        const gradeArr = Object.entries(cumulativeGrades).map(([grade, count]) => ({
+          grade,
+          count
+        }));
+
+        setTotalGradeData(gradeArr);
+      });
+  }, []);
 
   return (
-  <>
-    <div className="App">
-      <div className="courseInfo">
-        CMSC 216
-        <p className="subText">
-          Average GPA: {Math.round(data.average_gpa * 100) / 100}
-        </p>
-        <p className="subText">
-          Title: <i>{data.title}</i>
-        </p>
-      </div>
+    <>
+      <div className="App">
+        <div className="courseInfo">
+          CMSC 216
+          <p className="subText">
+            Average GPA: {Math.round(data.average_gpa * 100) / 100}
+          </p>
+          <p className="subText">
+            Title: <i>{data.title}</i>
+          </p>
+        </div>
 
-      <div className="charts">
-        <div className="chartsRow">
+        <div className="charts">
+          <div className="chartsRow">
             <TotalDistribution gradeData={totalGradeData} />
-            <GPATrend gradeData={totalGradeData}/>
-        </div>
-        <div className="sectionDist">
-          <SectionDistribution gradeData={gradeData} />
+            <GPATrend gradeData={totalGradeData} />
+          </div>
+          <div className="sectionDist">
+            <SectionDistribution gradeData={sectionData} />
+          </div>
         </div>
       </div>
-    </div>
-  </>
-)
+    </>
+  )
 }
 
 export default App
--- GPATrend/src/SectionDist.jsx
+++ GPATrend/src/SectionDist.jsx
@@ -5,53 +5,11 @@ import './App.css'
 import { useEffect, useState } from 'react'
 
 export function SectionDistribution({ gradeData }) {
-
-    const [sectionData, setSectionData] = useState([{}])
     const [currSectionDis, setCurrSectionDis] = useState([{}])
 
     useEffect(() => {
-        fetch("/course_grade_dis")
-            .then(res => res.json())
-            .then(data => {
-
-                let formatted_year = ""
-                let formatted_info = ""
-
-                const grade_dis = data.map(item => {
-
-                    const info = {
-                        "A": 0, "A+": 0, "A-": 0,
-                        "B": 0, "B+": 0, "B-": 0,
-                        "C": 0, "C+": 0, "C-": 0,
-                        "D": 0, "D+": 0, "D-": 0,
-                        "F": 0, "Other": 0, "W": 0,
-                        "course": "", "professor": "",
-                        "section": "", "semester": "",
-                        "formatted_name": ""
-                    };
-
-                    formatted_year = item.semester.slice(0, 4)
-                    if (item.semester[item.semester.length - 1] == "1")
-                        formatted_year = "Fall " + formatted_year
-                    else
-                        formatted_year = "Spring " + formatted_year
-
-                    formatted_info = formatted_year + ", " + item.section 
-                    + " - " + item.professor
-
-                    Object.keys(item).forEach(key => {
-                            info[key] = item[key]
-                            info["formatted_name"] = formatted_info
-                    })
-
-                    return info
-                })
-
-                setSectionData(grade_dis)
-                console.log(grade_dis)
-                set_section_grade_dis(0, grade_dis)
-            });
-    }, []);
+      set_section_grade_dis(0, gradeData);
+    }, [gradeData]);
 
     function set_section_grade_dis(index, data) {
 
@@ -68,7 +26,6 @@ export function SectionDistribution({ gradeData }) {
         }));
 
         setCurrSectionDis(gradeArray);
-        console.log(infoArray)
     }
 
     return (
@@ -76,8 +33,8 @@ export function SectionDistribution({ gradeData }) {
             <div className="dataCard">
                 <div>Grade Distribution by Section</div>
 
-                <select onChange={(e) => set_section_grade_dis(e.target.selectedIndex, sectionData)}>
-                    {sectionData.map((item, index) => (
+                <select onChange={(e) => set_section_grade_dis(e.target.selectedIndex, gradeData)}>
+                    {gradeData.map((item, index) => (
                         <option key={index} value={item.formatted_name}>
                             {item.formatted_name}
                         </option>
